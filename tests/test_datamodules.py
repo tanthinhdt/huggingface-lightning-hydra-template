1e2e7d5fd8368e9c0ee42b0bdcb6fca951730bc0
tanthinhdt <tanthinhdt24@gmail.com>
2026-03-03 16:18:37 +0900

    update(tests): update datamodule tests

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -1,9 +1,7 @@
-from pathlib import Path
-
 import pytest
 import torch
-
-from src.data.mnist_datamodule import MNISTDataModule
+from pathlib import Path
+from src.data.datamodule import DataModule
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
@@ -16,7 +14,7 @@ def test_mnist_datamodule(batch_size: int) -> None:
     """
     data_dir = "data/"
 
-    dm = MNISTDataModule(data_dir=data_dir, batch_size=batch_size)
+    dm = DataModule(data_dir=data_dir, batch_size=batch_size)
     dm.prepare_data()
 
     assert not dm.data_train and not dm.data_val and not dm.data_test
